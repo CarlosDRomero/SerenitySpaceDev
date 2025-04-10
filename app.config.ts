f876d48da1d1f8c0533977b2cbebdc9ce1fdbe42
CarlosDRomero <cdavid.romeror@gmail.com>
2025-04-10 16:01:41 -0500

    cambiar app.json por app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,52 @@
 export default {
-  googleWebClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID
+  googleWebClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+  "owner": "serenityspaceteam",
+  "name": "serenity-space-app",
+  "slug": "serenity-space-app",
+  "version": "1.0.0",
+  "orientation": "portrait",
+  "icon": "./assets/images/icon.png",
+  "scheme": "myapp",
+  "userInterfaceStyle": "automatic",
+  "newArchEnabled": true,
+  "ios": {
+    "supportsTablet": true,
+    "bundleIdentifier": "com.serenityspaceteam.app"
+  },
+  "android": {
+    "adaptiveIcon": {
+      "foregroundImage": "./assets/images/adaptive-icon.png",
+      "backgroundColor": "#ffffff"
+    },
+    "package": "com.serenityspaceteam.app"
+  },
+  "web": {
+    "bundler": "metro",
+    "output": "static",
+    "favicon": "./assets/images/favicon.png"
+  },
+  "plugins": [
+    "@react-native-google-signin/google-signin",
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        "image": "./assets/images/splash-icon.png",
+        "imageWidth": 200,
+        "resizeMode": "contain",
+        "backgroundColor": "#ffffff"
+      }
+    ]
+  ],
+  "experiments": {
+    "typedRoutes": true
+  },
+  "extra": {
+    "router": {
+      "origin": false
+    },
+    "eas": {
+      "projectId": "dd9c21e6-ccdf-4d33-a241-bf3da409c53c"
+    }
+  }
 }
