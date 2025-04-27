fbb1ee009f26a76844d1092a58155e5031939e58
CarlosDRomero <cdavid.romeror@gmail.com>
2025-04-27 11:32:52 -0500

    update call state

--- app/(videocalls)/_layout.tsx
+++ app/(videocalls)/_layout.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Button, Text } from "react-native";
 import { supabase } from "@/utils/supabase";
-import { StreamVideo, StreamCall, StreamVideoClient, StreamTheme, UI, CallControls, CallContent, VideoRenderer } from "@stream-io/video-react-native-sdk";
+import { StreamVideo, StreamCall, Call, StreamVideoClient, StreamTheme, CallContent, VideoRenderer } from "@stream-io/video-react-native-sdk";
 import BotonFormulario from "@/components/ui/BotonFormulario";
 
 
@@ -10,29 +10,33 @@ import BotonFormulario from "@/components/ui/BotonFormulario";
 export default function TestVideoModule() {
   const [streamToken, setStreamToken] = useState<string | null>(null);
   const [client, setClient] = useState<StreamVideoClient | null>(null);
-  const [call, setCall] = useState<any>(null);
+  const [call, setCall] = useState<Call | null>(null);
 
   // 1) Al montar, suscribirse al usuario
   useEffect(() => {
     supabase.auth.onAuthStateChange(async (_, session) => {
       if (session) {
         // 2) Llamar a la función que genera el token
-
-        
-        setStreamToken(session.access_token);
+        const {data, error} = await supabase.functions.invoke("get-stream-token")
+        if (error){
+          console.log("Error: ")
+          console.log(error)
+        }
+        setStreamToken(data.token);
+        console.log("Token:")
+        console.log(data)
         // 3) Inicializar cliente Stream Video
         const user = {
           id: session.user.id, name: session.user.email
         }
         const videoClient = new StreamVideoClient({
           apiKey: process.env.EXPO_PUBLIC_STREAM_VIDEO_API_KEY!,
-
         });
-        const {access_token,user: userG} = await videoClient.createGuestUser({user})
-        console.log(access_token)
+        // const {access_token,user: userG} = await videoClient.createGuestUser({user})
+        // console.log(access_token)
         await videoClient.connectUser(
-          userG,
-          access_token
+          user,
+          data.token
         );
         setClient(videoClient);
       }
@@ -43,11 +47,10 @@ export default function TestVideoModule() {
     if (!client) return;
 
     // 1) Crear/obtener la llamada con tipo y ID correctos
-    const activeCall = client.call("development", "test-call");
+    const activeCall = client.call("development", "new");
 
     // 2) Crear y unirte en un solo paso
     await activeCall.join({ create: true });
-    console.log("Connected?")
     // 3) Actualizar estado local
     setCall(activeCall);
   };
@@ -65,7 +68,10 @@ export default function TestVideoModule() {
           <StreamCall call={call}>
             <StreamTheme>
                 
-                <CallContent VideoRenderer={VideoRenderer}/>
+                <CallContent onHangupCallHandler={()=>{
+                  call.endCall()
+                  setCall(null)
+                }}/>
               
             </StreamTheme>
           </StreamCall>
