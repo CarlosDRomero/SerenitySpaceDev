ac906f4fb788a4c05b2f05b4d73f8f83ae52ff74
CarlosDRomero <cdavid.romeror@gmail.com>
2025-04-29 14:41:51 -0500

    dashboard

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -8,7 +8,6 @@ import {
   Dimensions,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import GruposIndex from '@/components/grupos/index';
 import ModuloGrupos from '@/components/ModuloGrupos/index';
 
 const { width, height } = Dimensions.get('window');
